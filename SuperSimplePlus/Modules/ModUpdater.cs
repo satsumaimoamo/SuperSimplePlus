5402efec01ab80b1af89d59a2bd9a37236782519
aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa9841 <98730287+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa9841@users.noreply.github.com>
2022-07-13 18:44:38 +0900

    完成

--- SuperSimplePlus/Modules/ModUpdater.cs
+++ SuperSimplePlus/Modules/ModUpdater.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -16,6 +17,7 @@ namespace SuperSimplePlus.Modules
         public static string Tag;
         public static JObject data;
         public static GenericPopup popup;
+        public static GameObject PopupButton;
 
         public async static Task<bool> IsNewer()//最新版かどうか判定
         {
@@ -36,40 +38,48 @@ namespace SuperSimplePlus.Modules
         }
         public async static Task<bool> DownloadUpdate()
         {
-            popup = UnityEngine.Object.Instantiate(TwitchManager.Instance.TwitchPopup);
-            popup.TextAreaTMP.fontSize *= 0.7f;
-            popup.TextAreaTMP.enableAutoSizing = false;
-
-            popup.Show();
-
-            var button = popup.transform.GetChild(2).gameObject;
-            button.SetActive(false);
-            popup.TextAreaTMP.text = $"SuperSimplePlusをアップデート中\nしばらくお待ちください";
-
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", "SuperSimplePlus Updater");
-
-            JToken assets = data["assets"];
-            string downloadURI = "";
-            string browser_download_url = assets.First["browser_download_url"]?.ToString();
-            if (browser_download_url != null && assets.First["content_type"] != null)
+            try
             {
-                if (assets.First["content_type"].ToString().Equals("application/x-msdownload") &&
-                    browser_download_url.EndsWith(".dll"))
+                PopupButton = popup.transform.GetChild(2).gameObject;
+                PopupButton.SetActive(false);
+                popup.TextAreaTMP.text = $"SuperSimplePlusをアップデート中\nしばらくお待ちください";
+
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("User-Agent", "SuperSimplePlus Updater");
+
+                JToken assets = data["assets"];
+                string downloadURI = "";
+
+                for (JToken current = assets.First; current != null; current = current.Next)
                 {
-                    downloadURI = browser_download_url;
+                    string browser_download_url = current["browser_download_url"]?.ToString();
+                    if (browser_download_url != null && current["content_type"] != null)
+                    {
+                        if (current["content_type"].ToString().Equals("application/x-msdownload") &&
+                            browser_download_url.EndsWith(".dll"))
+                        {
+                            downloadURI = browser_download_url;
+                        }
+                    }
                 }
+
+                if (downloadURI.Length == 0) return false;
+
+                var res = await client.GetAsync(downloadURI, HttpCompletionOption.ResponseContentRead);
+                string filePath = Path.Combine(Paths.PluginPath, "SuperSimplePlus");
+                if (File.Exists(filePath + ".old")) File.Delete(filePath + ".old");
+                if (File.Exists(filePath)) File.Move(filePath, filePath + ".old");
+
+                await using var responseStream = await res.Content.ReadAsStreamAsync();
+                await using var fileStream = File.Create(filePath);
+                await responseStream.CopyToAsync(fileStream);
+
+                PopupButton.SetActive(true);
             }
-            if (downloadURI.Length == 0) return false;
-
-            var res = await client.GetAsync(downloadURI, HttpCompletionOption.ResponseContentRead);
-            string filePath = Path.Combine(Paths.PluginPath, "SuperSimplePlus.dll");
-            if (File.Exists(filePath + ".old")) File.Delete(filePath + ".old");
-            if (File.Exists(filePath)) File.Move(filePath, filePath + ".old");
-
-            await using var responseStream = await res.Content.ReadAsStreamAsync();
-            await using var fileStream = File.Create(filePath);
-            await responseStream.CopyToAsync(fileStream);
+            catch (Exception e) {
+                SSPPlugin.Logger.LogError($"[ModUpdater:Error]{e}");
+                return false;
+            }
 
             return true;
         }
@@ -82,22 +92,33 @@ namespace SuperSimplePlus.Modules
                 Task<bool> SSPUpdateCheck = Task.Run(IsNewer);
                 if (SSPUpdateCheck.Result) //最新版じゃなかったらボタンを作る
                 {
+                    string filePath = Path.Combine(Paths.PluginPath, "SuperSimplePlus.dll");
+                    if (File.Exists(filePath + ".old")) File.Delete(filePath + ".old");
+
+                    popup = UnityEngine.Object.Instantiate(TwitchManager.Instance.TwitchPopup);
+                    popup.TextAreaTMP.fontSize *= 0.7f;
+                    popup.TextAreaTMP.enableAutoSizing = false;
+
                     var template = GameObject.Find("ExitGameButton");
                     if (template == null) return;
 
                     var buttonSSPUpdate = UnityEngine.Object.Instantiate(template, null);
-                    buttonSSPUpdate.transform.localPosition = new Vector3(buttonSSPUpdate.transform.localPosition.x, buttonSSPUpdate.transform.localPosition.y + 0.6f, buttonSSPUpdate.transform.localPosition.z);
+                    buttonSSPUpdate.transform.localPosition = new Vector3(buttonSSPUpdate.transform.localPosition.x + 9.4f, buttonSSPUpdate.transform.localPosition.y + 4.8f, buttonSSPUpdate.transform.localPosition.z);
 
                     var textSSPUpdateButton = buttonSSPUpdate.transform.GetChild(0).GetComponent<TMPro.TMP_Text>();
                     __instance.StartCoroutine(Effects.Lerp(0.1f, new System.Action<float>((p) =>
                     {
-                        textSSPUpdateButton.SetText("<color=#ff0000>SuperSimplePlus</color>をアップデートする");
+                        textSSPUpdateButton.SetText($"{SSPPlugin.ColoredModName}\nをアップデートする");
                     })));
 
                     PassiveButton passiveButtonSSPUpdate = buttonSSPUpdate.GetComponent<PassiveButton>();
 
                     passiveButtonSSPUpdate.OnClick = new Button.ButtonClickedEvent();
-                    passiveButtonSSPUpdate.OnClick.AddListener((System.Action)(() => popup.TextAreaTMP.text = Task.Run(DownloadUpdate).Result  ? "SuperSimplePlusが正常に更新されました。\nゲームを再起動してください" : "アップデートに失敗しました\n後で再試行してください。\nまたは手動でアップデートしてください。"));
+                    passiveButtonSSPUpdate.OnClick.AddListener((System.Action)(() => {
+                        popup.Show();
+                        popup.TextAreaTMP.text = Task.Run(DownloadUpdate).Result ? $"{SSPPlugin.ColoredModName}が正常に更新されました。\nゲームを再起動してください" : "アップデートに失敗しました\n後で再試行してください。\nまたは手動でアップデートしてください。";
+                        PopupButton.SetActive(true);
+                    }));
                 }
             }
         }
