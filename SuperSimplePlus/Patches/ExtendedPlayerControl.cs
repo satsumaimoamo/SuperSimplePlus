4e49b7e995c2822b28788fe2f425996e94a6da61
satsumaimoamo <meiabao25@gmail.com>
2022-07-14 22:16:47 +0900

    会議開始機能追加

---
+++ SuperSimplePlus/Patches/ExtendedPlayerControl.cs
@@ -0,0 +1,13 @@
+namespace SuperSimplePlus.Patches
+{
+    public static class ExtendedPlayerControl
+    {
+        public static void NoCheckStartMeeting(this PlayerControl reporter, GameData.PlayerInfo target)
+        { /*サボタージュ中でも関係なしに会議を起こせるメソッド
+            targetがnullの場合はボタンとなる*/
+            MeetingRoomManager.Instance.AssignSelf(reporter, target);
+            DestroyableSingleton<HudManager>.Instance.OpenMeetingRoom(reporter);
+            reporter.RpcStartMeeting(target);
+        }
+    }
+}
