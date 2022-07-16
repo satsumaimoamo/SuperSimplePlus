79e4b6ec49a556985aa5d6eae4d5c583d4e77668
aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa9841 <98730287+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa9841@users.noreply.github.com>
2022-07-16 10:27:15 +0900

    完成

---
+++ SuperSimplePlus/Patches/ClientOptionsPatch.cs
@@ -0,0 +1,168 @@
+using HarmonyLib;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.Events;
+using static UnityEngine.UI.Button;
+using Object = UnityEngine.Object;
+
+namespace SuperSimplePlus.Patches
+{
+    [HarmonyPatch]
+    public static class ClientOptionsPatch
+    {
+        private static SelectionBehaviour[] AllOptions = {
+            new SelectionBehaviour("DebugModeButton", () => SSPPlugin.debugTool.Value = !SSPPlugin.debugTool.Value, SSPPlugin.debugTool.Value),
+        };
+
+        public static GameObject SSPSettingButton;
+
+        public static GameObject SSPOptionsMenu;
+        private static TextMeshPro titleText;
+
+        private static ToggleButtonBehaviour buttonPrefab;
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+        public static void MainMenuManager_StartPostfix(MainMenuManager __instance)
+        {
+            // Prefab for the title
+            var tmp = __instance.Announcement.transform.Find("Title_Text").gameObject.GetComponent<TextMeshPro>();
+            tmp.alignment = TextAlignmentOptions.Center;
+            tmp.transform.localPosition += Vector3.left * 0.2f;
+            titleText = Object.Instantiate(tmp);
+            Object.Destroy(titleText.GetComponent<TextTranslatorTMP>());
+            titleText.gameObject.SetActive(false);
+            Object.DontDestroyOnLoad(titleText);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HudManager), nameof(HudManager.Start))]
+        public static void HudManager_StartPostfix(HudManager __instance)
+        {
+            SSPSettingButton = GameObject.Instantiate(HudManager.Instance.MapButton.gameObject);
+            SSPSettingButton.transform.SetParent(HudManager.Instance.transform);
+
+            SSPSettingButton.SetActive(true);
+
+            SSPSettingButton.GetComponent<ButtonBehavior>().OnClick.RemoveAllListeners();
+            SSPSettingButton.GetComponent<ButtonBehavior>().OnClick.AddListener((UnityAction)(() => { SSPSettingButtonOnClick(); }));
+
+            buttonPrefab = Object.Instantiate(HudManager.Instance.transform.FindChild("Menu").GetComponent<OptionsMenuBehaviour>().CensorChatButton);
+            Object.DontDestroyOnLoad(buttonPrefab);
+            buttonPrefab.name = "CensorChatPrefab";
+            buttonPrefab.gameObject.SetActive(false);
+        }
+
+        private static void SSPSettingButtonOnClick()
+        {
+            if (SSPOptionsMenu) return;
+
+            SSPOptionsMenu = Object.Instantiate(HudManager.Instance.transform.FindChild("Menu").GetComponent<OptionsMenuBehaviour>().Background.gameObject);
+            SSPOptionsMenu.transform.SetParent(HudManager.Instance.transform);
+            SSPOptionsMenu.transform.position = HudManager.Instance.transform.FindChild("Menu").GetComponent<OptionsMenuBehaviour>().transform.position;
+            SSPOptionsMenu.transform.localScale = new(SSPOptionsMenu.transform.localScale.x * 0.9f, SSPOptionsMenu.transform.localScale.y * 0.9f, SSPOptionsMenu.transform.localScale.z);
+
+            GameObject SSPOptionsMenuCloseButton = GameObject.Instantiate(HudManager.Instance.MapButton.gameObject);
+            SSPOptionsMenuCloseButton.SetActive(true);
+            SSPOptionsMenuCloseButton.transform.SetParent(SSPOptionsMenu.transform);
+            SSPOptionsMenuCloseButton.transform.position = new(0.5f,4.7f,SSPOptionsMenuCloseButton.transform.position.z);
+            SSPOptionsMenuCloseButton.GetComponent<SpriteRenderer>().sortingOrder = 1;
+            SSPOptionsMenuCloseButton.GetComponent<SpriteRenderer>().sprite = GetCloseButtonSprite();
+            SSPOptionsMenuCloseButton.GetComponent<ButtonBehavior>().OnClick.RemoveAllListeners();
+            SSPOptionsMenuCloseButton.GetComponent<ButtonBehavior>().OnClick.AddListener((UnityAction)(() =>
+            {
+                GameObject.Destroy(SSPOptionsMenu);
+                PlayerControl.LocalPlayer.moveable = true;
+            }));
+
+            var title = Object.Instantiate(titleText, SSPOptionsMenu.transform);
+            title.GetComponent<RectTransform>().localPosition = Vector3.up * 2.3f;
+            title.gameObject.SetActive(true);
+            title.text = $"{SSPPlugin.ColoredModName}の設定";
+            title.name = "TitleText";
+
+            SetUpOptions();
+        }
+
+
+        private static Sprite CloseButtonSprite;
+        public static Sprite GetCloseButtonSprite()
+        {
+            if (CloseButtonSprite) return CloseButtonSprite;
+            CloseButtonSprite = Helpers.loadSpriteFromResources("SuperSimplePlus.Resources.CloseButton.png", 115f);
+            return CloseButtonSprite;
+        }
+
+        private static void SetUpOptions()
+        {
+            for (var i = 0; i < AllOptions.Length; i++)
+            {
+                var info = AllOptions[i];
+
+                var button = Object.Instantiate(buttonPrefab, SSPOptionsMenu.transform);
+
+                var pos = new Vector3(i % 2 == 0 ? -1.17f : 1.17f, 1.3f - i / 2 * 0.8f, -.5f);
+
+                var transform = button.transform;
+                transform.localPosition = pos;
+
+                button.onState = info.DefaultValue;
+                button.Background.color = button.onState ? Color.green : Palette.ImpostorRed;
+
+                button.Text.text = info.Title;
+                button.Text.fontSizeMin = button.Text.fontSizeMax = 2.5f;
+                button.Text.font = Object.Instantiate(titleText.font);
+                button.Text.GetComponent<RectTransform>().sizeDelta = new Vector2(2, 2);
+
+                button.name = info.Title.Replace(" ", "") + "Toggle";
+                button.gameObject.SetActive(true);
+
+                var passiveButton = button.GetComponent<PassiveButton>();
+                var colliderButton = button.GetComponent<BoxCollider2D>();
+
+                colliderButton.size = new Vector2(2.2f, .7f);
+
+                passiveButton.OnClick = new ButtonClickedEvent();
+                passiveButton.OnMouseOut = new UnityEvent();
+                passiveButton.OnMouseOver = new UnityEvent();
+
+                passiveButton.OnClick.AddListener((Action)(() =>
+                {
+                    button.onState = info.OnClick();
+                    button.Background.color = button.onState ? Color.green : Palette.ImpostorRed;
+                }));
+
+                passiveButton.OnMouseOver.AddListener((Action)(() => button.Background.color = new Color32(34, 139, 34, byte.MaxValue)));
+                passiveButton.OnMouseOut.AddListener((Action)(() => button.Background.color = button.onState ? Color.green : Palette.ImpostorRed));
+
+                foreach (var spr in button.gameObject.GetComponentsInChildren<SpriteRenderer>())
+                    spr.size = new Vector2(2.2f, .7f);
+            }
+        }
+
+        private static IEnumerable<GameObject> GetAllChilds(this GameObject Go)
+        {
+            for (var i = 0; i < Go.transform.childCount; i++)
+            {
+                yield return Go.transform.GetChild(i).gameObject;
+            }
+        }
+
+        public class SelectionBehaviour
+        {
+            public string Title;
+            public Func<bool> OnClick;
+            public bool DefaultValue;
+
+            public SelectionBehaviour(string title, Func<bool> onClick, bool defaultValue)
+            {
+                Title = title;
+                OnClick = onClick;
+                DefaultValue = defaultValue;
+            }
+        }
+    }
+}
